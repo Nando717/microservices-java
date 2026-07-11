cc9523b0f91676628fb9f713230606505db512a5
Nando717 <iverlandioedsonn@gmail.com>
2026-07-10 22:05:22 -0300

    criando primeira classe de usuarios

---
+++ user/user/src/main/java/com/ms/user/Entity/Users.java
@@ -0,0 +1,28 @@
+package com.ms.user.Entity;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Table(name = "TB_USERS")
+@Entity
+class Users implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+
+    private UUID userId;
+
+    @Column(nullable = false, length =  150, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String age;
+
+}
