cb26b32143b06dd3b30d1874c9df22539a1ae8a3
Nando717 <iverlandioedsonn@gmail.com>
2026-07-23 21:55:36 -0300

    implementando metodo get

--- user/user/src/main/java/com/ms/user/service/UserService.java
+++ user/user/src/main/java/com/ms/user/service/UserService.java
@@ -3,12 +3,11 @@ package com.ms.user.service;
 import com.ms.user.models.UserModel;
 import com.ms.user.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class userService {
+public class UserService {
 
     @Autowired
     UserRepository userRepository;
